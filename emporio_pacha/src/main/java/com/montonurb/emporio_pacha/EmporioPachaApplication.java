8002aa24f9adb0e94fa8ed1ca6419ac1b0f8de74
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2024-02-16 00:56:20 -0300

    Adicionando primeiro projeto

---
+++ emporio_pacha/src/main/java/com/montonurb/emporio_pacha/EmporioPachaApplication.java
@@ -0,0 +1,13 @@
+package com.montonurb.emporio_pacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmporioPachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmporioPachaApplication.class, args);
+	}
+
+}
---
+++ emporio_pacha/src/test/java/com/montonurb/emporio_pacha/EmporioPachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.montonurb.emporio_pacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmporioPachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
