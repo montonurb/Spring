be5cc2269d0d74b631466e8327ef40c86c2897c1
Bruno Monteiro <montonurb@MacBook-Air-de-Bruno.local>
2024-02-27 08:27:15 -0300

    Criando projeto

---
+++ gestao_vagas/src/main/java/br/com/montonurb/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -0,0 +1,10 @@
+package br.com.montonurb.gestao_vagas.modules.candidate.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/candidate")
+public class CandidateController {
+    
+}
